d9488a5dc109bae9337c27429a2f8d14a660f7fe
Ethan Moskowitz <ethanmoskowitz0@gmail.com>
2026-04-22 16:53:25 -0400

    Quiz Detail Page

--- app/(kambaz)/courses/[cid]/Breadcrumb.tsx
+++ app/(kambaz)/courses/[cid]/Breadcrumb.tsx
@@ -3,17 +3,21 @@ import React from "react";
 import { usePathname } from "next/navigation";
 export default function Breadcrumb({
   course,
+  quiz,
 }: {
   course: { name: string } | undefined;
+  quiz?: { title: string };
 }) {
   const pathname = usePathname();
   return (
     <span>
       Course {course?.name} &gt;{" "}
-      {pathname
-        .split("/")
-        .pop()
-        ?.replace(/^\w/, (c) => c.toUpperCase())}
+      {pathname.includes("quizzes/")
+        ? quiz?.title
+        : pathname
+            .split("/")
+            .pop()
+            ?.replace(/^\w/, (c) => c.toUpperCase())}
     </span>
   );
 }
